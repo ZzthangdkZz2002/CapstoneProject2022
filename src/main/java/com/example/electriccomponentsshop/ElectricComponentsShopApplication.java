ec7807ea6430a64ffd80bf9cd52c8e7fdf081067
DESKTOP-4IR8GTN\User <thangnv82@viettel.com.vn>
2022-09-17 06:01:37 +0700

    change plugin config

--- src/main/java/com/example/electriccomponentsshop/ElectricComponentsShopApplication.java
+++ src/main/java/com/example/electriccomponentsshop/ElectricComponentsShopApplication.java
@@ -2,10 +2,10 @@ package com.example.electriccomponentsshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ElectricComponentsShopApplication{
+public class ElectricComponentsShopApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ElectricComponentsShopApplication.class, args);
